3aeb812c5de7bcb3d0ce14622061a995fe26b4c6
Tosin Obasa <eventide026@gmail.com>
2025-12-24 07:42:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9021474887-8f608",
+  "appId": "1:840041623104:web:a1b205448e9dcf616339a5",
+  "apiKey": "AIzaSyCpSYpqhqZchAyUzGs5o_LnnLyHqqkjY7w",
+  "authDomain": "studio-9021474887-8f608.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "840041623104"
+};
