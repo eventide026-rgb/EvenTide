529b61c7f92971843a49ee3b7d006029ff70b0da
Tosin Obasa <eventide026@gmail.com>
2026-03-10 19:35:27 +0000

    fix: add africastalking types declaration

--- src/types/africastalking.d.ts
+++ src/types/africastalking.d.ts
@@ -1,17 +1,1 @@
-declare module "africastalking" {
-  interface SMS {
-    send(options: { to: string | string[]; message: string }): Promise<any>;
-  }
-
-  interface Africastalking {
-    SMS: SMS;
-    PAYLOADS?: {
-      WhatsApp: any;
-    };
-    AIRTIME?: any;
-  }
-
-  function Africastalking(options: { apiKey: string; username: string }): Africastalking;
-
-  export = Africastalking;
-}
+declare module "africastalking";
