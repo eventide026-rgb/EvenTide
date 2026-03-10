8891714f155d29bf8339631d4f3acc9ba7a2fafe
Tosin Obasa <eventide026@gmail.com>
2026-03-10 19:37:22 +0000

    fix: add africastalking types

--- src/types/africastalking.d.ts
+++ src/types/africastalking.d.ts
@@ -1,1 +1,26 @@
-declare module "africastalking";
+
+declare module "africastalking" {
+    interface AfricasTalkingConfig {
+      apiKey: string;
+      username: string;
+    }
+  
+    interface SMSOptions {
+      to: string[];
+      message: string;
+    }
+  
+    interface SMSResponse {
+      SMSMessageData: any;
+    }
+  
+    interface AfricasTalkingInstance {
+      SMS: {
+        send(options: SMSOptions): Promise<SMSResponse>;
+      };
+    }
+  
+    export default function AfricasTalking(
+      config: AfricasTalkingConfig
+    ): AfricasTalkingInstance;
+  }
